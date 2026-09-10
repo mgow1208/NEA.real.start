250dd41f5d11a2ef840112a25df875d7ca764b5c
mgow1208 <27franklandc@wirralgrammarboys.com>
2026-09-10 10:21:16 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,1 +1,12 @@
 # NEA.real.start
+import csv
+import urllib.request
+DataRepository = "https://football-data.co.uk/mmz4281/2526/E0.csv"
+rawData = urllib.request.urlopen(DataRepository)
+textLines = [line.decode('utf-8') for line in rawData.readlines()]
+Rows = csv.DictReader(textLines)
+
+
+
+
+#https://dashee87.github.io/football/python/predicting-football-results-with-statistical-modelling-dixon-coles-and-time-weighting/
